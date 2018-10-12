c1b18739d107e4c7fbf78315836bc80a5f4c27c8
C-Low3 <RLCritchlow.III@gmail.com>
2018-10-11 22:21:40 -0400

    startup2

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "msp.h"
+
+
+/**
+ * Authors: Richard Critchlow & Logan Dykstra
+ * Version: October 2018
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+}
